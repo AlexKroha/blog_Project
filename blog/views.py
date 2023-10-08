0b2ce6fb39c0b10cfb5b8d10543ea70688d1c59b
AlexKroha <sash2005-3@mail.ru>
2023-10-08 22:18:21 +0300

    commit init

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+# blog/views.py
+from django.views.generic import ListView, DetailView  # new
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+
+
+class BlogDetailView(DetailView):  # new
+    model = Post
+    template_name = "blog/post_detail.html"

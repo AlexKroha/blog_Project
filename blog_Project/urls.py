d100ff688ad53fe208e878ddc331b0546ac409e3
AlexKroha <sash2005-3@mail.ru>
2023-10-11 20:42:12 +0300

    Created Account

--- blog_Project/urls.py
+++ blog_Project/urls.py
@@ -1,8 +1,10 @@
 # django_project/urls.py
 from django.contrib import admin
-from django.urls import path, include  # new
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls")),  # new
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),  # new
+    path("", include("blog.urls")),
 ]
